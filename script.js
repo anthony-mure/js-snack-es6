df1e80e136466bda69385317d189a05a170f8284
anthony-murè <tony.mure99@gmail.com>
2025-07-14 15:37:39 +0200

    [add]snack1(array-bikes)

--- script.js
+++ script.js
@@ -1,5 +1,36 @@
+
 /*Snack 1
 
 Creare un array di oggetti:
 Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
 Stampare a schermo la bici con peso minore. */
+
+const bikes = [
+  {
+    name : 'bianchi',
+    kilogram : '6.8'
+  },
+  {
+    name : 'pinarello',
+    kilogram : '7.0'
+  },
+  {
+    name : 'deRosa',
+    kilogram : '6.9'
+  },
+  {
+    name : 'trek',
+    kilogram : '7.2'
+  },
+  {
+    name : 'scott',
+    kilogram : '7.8'
+  },
+  {
+    name : 'canyon',
+    kilogram : '7.5'
+  }
+];
+
+console.log(bikes);
+    
