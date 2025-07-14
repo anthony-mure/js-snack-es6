3011cdb39d8967b3ab3d2973e0f096bb470bf56e
anthony-murè <tony.mure99@gmail.com>
2025-07-14 17:02:22 +0200

    [add]snack2(array-football-teams)

--- script.js
+++ script.js
@@ -53,4 +53,58 @@ console.log(lightWeight);
 
 
 
-/* */
+/*Snack2
+
+Creare un array di oggetti di squadre di calcio.
+Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
+Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
+Generare numeri random al posto degli 0 nelle proprietà “punti” fatti e “falli subiti”.
+Infine, creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console. */
+
+
+const footballTeams = [
+  {
+    name : 'milan',
+    pointsMade : 0,
+    foulsDrawn : 0
+  },
+  {
+    name : 'inter',
+    pointsMade : 0,
+    foulsDrawn : 0
+  },
+  {
+    name : 'roma',
+    pointsMade : 0,
+    foulsDrawn : 0
+  },
+  {
+    name : 'atalanta',
+    pointsMade : 0,
+    foulsDrawn : 0
+  },
+  {
+    name : 'napoli',
+    pointsMade : 0,
+    foulsDrawn : 0
+  },
+  {
+    name : 'fiorentina',
+    pointsMade : 0,
+    foulsDrawn : 0
+  },
+  {
+    name : 'bologna',
+    pointsMade : 0,
+    foulsDrawn : 0
+  },
+  {
+    name : 'cagliari',
+    pointsMade : 0,
+    foulsDrawn : 0
+  },
+];
+
+console.log(footballTeams);
+
+//Generare numeri random al posto degli 0 nelle proprietà “punti” fatti e “falli subiti”.
